941a90ca1afa28a977cb7e72f95a07fc7f1de742
noor-eldeen <nooreldeensayed7@gmail.com>
2026-07-09 01:28:41 +0300

    Update card HTML structure and button text

--- js/card.js
+++ js/card.js
@@ -4,9 +4,9 @@ azkar.forEach(zekr => {
     <div class="card p-20 rad-6" data-repeat="${zekr.repeat}">
         <h3 class="m-none">${zekr.main}</h3>
         <p class="m-none txt">${zekr.info}</p>
-        <p class="m-none txt repeat-text"></p>
+        <p class="m-none txt repeat-text w-fit"></p>
         <div class="counter-holder m-c w-fit flex-align-c">
-            <button class="taspeh">تسبيح</button>
+            <button class="taspeh">تكرار</button>
             <span class="num">0</span>
             <button class="reset">إعادة</button>
         </div>
